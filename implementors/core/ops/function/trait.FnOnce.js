623276692e620519696d1cc98de2a91a198f2e1b
Travis CI User <travis@example.org>
2017-12-22 12:54:11 +0000

    Documentation for japaric/criterion.rs@1c83583

---
+++ implementors/core/ops/function/trait.FnOnce.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["criterion_stats"] = ["impl&lt;A&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/function/trait.FnOnce.html\" title=\"trait core::ops::function::FnOnce\">FnOnce</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">(</a>A<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">,)</a>&gt; for <a class=\"struct\" href=\"criterion_stats/univariate/kde/kernel/struct.Gaussian.html\" title=\"struct criterion_stats::univariate::kde::kernel::Gaussian\">Gaussian</a> <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;A: Float,&nbsp;</span>","impl&lt;'a, A, K&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/function/trait.FnOnce.html\" title=\"trait core::ops::function::FnOnce\">FnOnce</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">(</a>A<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">,)</a>&gt; for <a class=\"struct\" href=\"criterion_stats/univariate/kde/struct.Kde.html\" title=\"struct criterion_stats::univariate::kde::Kde\">Kde</a>&lt;'a, A, K&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;A: 'a + Float,<br>&nbsp;&nbsp;&nbsp;&nbsp;K: <a class=\"trait\" href=\"criterion_stats/univariate/kde/kernel/trait.Kernel.html\" title=\"trait criterion_stats::univariate::kde::kernel::Kernel\">Kernel</a>&lt;A&gt;,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
